fc582759cd8b87a9f387ccd943db045894929344
Richard L. Sypert Jr <rsypertjr@Ubuntu-20.04-for-docker>
2025-02-10 16:23:39 +0000

    update

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,26 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.jsx',
+            input: ['resources/css/app.css','resources/js/app.jsx'],
             refresh: true,
         }),
         react(),
     ],
+    server: {
+      hmr: {
+          host: 'localhost',
+      },
+  },
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: ['resources/js/app.jsx', './index.html']
+    }
+  }
 });
