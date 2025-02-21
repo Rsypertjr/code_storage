40739bdb1afa9462c979f6202a7099a59c9c54ea
Richard L. Sypert Jr <rsypertjr@hotmail.com>
2025-02-20 16:01:42 -0800

    eliminate topnave separate component

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,7 +1,8 @@
 import './bootstrap';
 import '../css/app.css';
-import '../css/portfolio.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+import '../css/portfolio.css';
 
 import { createRoot } from 'react-dom/client';
 import { createInertiaApp } from '@inertiajs/react';
