f201948a15caece99515912f0231d4887b689ed6
Richard L. Sypert Jr <rsypertjr@hotmail.com>
2025-10-04 05:38:41 +0000

    update titles, add ai copilot next.js vote parser

--- resources/js/Pages/Portfolio.jsx
+++ resources/js/Pages/Portfolio.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+
+import { Head } from '@inertiajs/react';
 import { Container, Row, Col, Nav, Navbar, CardText, Tooltip, OverlayTrigger} from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import Card from 'react-bootstrap/Card';
@@ -23,10 +25,16 @@ import mobiledevelopmentimage from '../../images/mobiledevelopmentimage.jpg';
 import tictactoe from '../../images/tictactoe.png';
 import mern from '../../images/mern.jpeg';
 import vegas from '../../images/vegas/vegas6.jpg';
+import { set } from 'lodash';
 
 export default function Portfolio(props){  
     const [putLampCover, setPutLampCover] = useState(false);
-
+    const [pageTitle, setPageTitle] = useState("Work Portfolio");
+    
+      useEffect(() => {
+          document.title = pageTitle || 'Work Portfolio'; // Set the title
+          console.log("App Router page title:",pageTitle);
+      },[pageTitle]);
 
     const hideLampCover = (e) => {
         if($('#lampCover').css('visibility') == 'visible')
@@ -99,8 +107,9 @@ export default function Portfolio(props){
             $('#homeCover').css('visibility','visible');
     };
 
-    const selectNavItem = (e) => {
+    const selectNavItem = (e, pTitle) => {
       //console.log(e.target);
+      setPageTitle(pTitle);
       var targetEl = $(e.target.getAttribute('href'));
       if (targetEl.length) {
         e.preventDefault();
@@ -187,43 +196,41 @@ export default function Portfolio(props){
 					
            
 			<Container style={{width:"95%"}} fluid>
-                 <TopCarousel/> 
-                 <Container className="d-flex justify-content-center" style={{backgroundColor:"black"}} fluid>
-                 <Navbar expand="lg"  style={{zIndex:200}} className="bg-body-tertiary d-flex justify-content-center" id="topNav">  {/* Beginning of Navigation */}
+                <TopCarousel pageTitle={pageTitle}/> 
+                <Container className="d-flex justify-content-center" style={{backgroundColor:"black"}} fluid>
+                    <Navbar expand="lg"  style={{zIndex:200}} className="bg-body-tertiary d-flex justify-content-center" id="topNav">  {/* Beginning of Navigation */}
                     {/*<a className="navbar-brand" href="#">Bootstrap Work Portfolio</a>*/}
                     <Container>
-                        <Navbar.Brand href="/#home" onClick={(e) => selectNavItem(e)}>Bootstrap Work Portfolio</Navbar.Brand>
+                        <Navbar.Brand href="/#home" onClick={(e) => selectNavItem(e,"Home")}>Bootstrap Work Portfolio</Navbar.Brand>
                         <Navbar.Toggle aria-controls="basic-navbar-nav" />
                         {/*<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>*/}
                         <Navbar.Collapse id="myNavbar">
                             <Nav className="me-auto">
-                                <Nav.Link href="#home" onClick={selectNavItem}>Home/Code Repos</Nav.Link>
-                                <Nav.Link href="#about" onClick={(e) => selectNavItem(e)}>About</Nav.Link>
+                                <Nav.Link href="#home" onClick={(e) => selectNavItem(e,"Home/Code Repos")}>Home/Code Repos</Nav.Link>
+                                <Nav.Link href="#about" onClick={(e) => selectNavItem(e,"About")}>About</Nav.Link>
                                 <NavDropdown title="Software Development" id="basic-nav-dropdown">
-                                    <NavDropdown.Item href="#lamp" onClick={(e) => selectNavItem(e)}>LAMP - based</NavDropdown.Item>
+                                    <NavDropdown.Item href="#lamp" onClick={(e) => selectNavItem(e,"LAMP - based")}>LAMP - based</NavDropdown.Item>
                                     <NavDropdown.Divider />
-                                    <NavDropdown.Item href="#mobile" onClick={(e) => selectNavItem(e)}>JQuery Mobile</NavDropdown.Item>
+                                    <NavDropdown.Item href="#mobile" onClick={(e) => selectNavItem(e,"JQuery Mobile")}>JQuery Mobile</NavDropdown.Item>
                                     <NavDropdown.Divider />
-                                    <NavDropdown.Item href="#frameworks" onClick={(e) => selectNavItem(e)}>Frameworks</NavDropdown.Item>
+                                    <NavDropdown.Item href="#frameworks" onClick={(e) => selectNavItem(e,"Frameworks")}>Frameworks</NavDropdown.Item>
                                 </NavDropdown>
                                 <NavDropdown title="Technical Writing" id="basic-nav-dropdown">
-                                    <NavDropdown.Item href="#manuals" onClick={(e) => selectNavItem(e)}>Production and Maintenance Manuals</NavDropdown.Item>
+                                    <NavDropdown.Item href="#manuals" onClick={(e) => selectNavItem(e,"Production and Maintenance Manuals")}>Production and Maintenance Manuals</NavDropdown.Item>
                                     <NavDropdown.Divider />
-                                    <NavDropdown.Item href="#specifications" onClick={(e) => selectNavItem(e)} >Technical Specification Manuals</NavDropdown.Item>
+                                    <NavDropdown.Item href="#specifications" onClick={(e) => selectNavItem(e,"Technical Specification Manuals")}>Technical Specification Manuals</NavDropdown.Item>
                                 </NavDropdown>
-                                <Nav.Link href="#resume" onClick={(e) => selectNavItem(e)}>My Resume</Nav.Link>
-                                <Nav.Link href="#living" onClick={(e) => selectNavItem(e)}>Living In Vegas</Nav.Link>
-                                <Nav.Link href="#front" onClick={(e) => selectNavItem(e)}>Non-Bootstrap Portfolio</Nav.Link>               
+                                <Nav.Link href="#resume" onClick={(e) => selectNavItem(e,"My Resume")}>My Resume</Nav.Link>
+                                <Nav.Link href="#living" onClick={(e) => selectNavItem(e,"Living In Vegas")}>Living In Vegas</Nav.Link>
+                                <Nav.Link href="#front" onClick={(e) => selectNavItem(e,"Non-Bootstrap Portfolio")}>Non-Bootstrap Portfolio</Nav.Link>               
                             </Nav>
-                        </Navbar.Collapse>
-                        
-                       
+                        </Navbar.Collapse> 
                     </Container>
                    
-            </Navbar>    {/*---- End of Navigation Header ---------------------------------*/}	
-                 </Container>
+                </Navbar>    {/*---- End of Navigation Header ---------------------------------*/}	
+            </Container>
                  <MidCarousel/>
                  
             </Container>
@@ -413,7 +420,16 @@ export default function Portfolio(props){
             <section id="frameworks">
             <OverlayTrigger placement="top" overlay={(tooltip("Click to Toggle Cover Layer"))}>
                 <Container  style={{backgroundColor:'#6A5ACD'}} className="panel-container" onClick={hideFrameworksCover} fluid>
-                    
+                    <Card style={{backgroundColor:'#b3f5daff'}}>
+                        <img src={tictactoe} className="rounded mb-2" height="15%" width="15%" style={{margin:'1em 0 0 42.5%'}} alt="Tic Tac Toe Image"/> 
+                        <Card.Body>             
+                            <Card.Link href="https://codestorage.vercel.app/" target="_blank">AI-CoPilot-Next.js-Suprabase-Voting Analyzer</Card.Link>
+                            <Card.Text><object data="https://codestorage.vercel.app/" height="200" width="300"></object></Card.Text>
+                            <Card.Text className="text-center app-description">New Next.js-AI-CoPilot Developed Presidential Voting Analyzer.&nbsp;&nbsp;Supabase SQL Integration for Front-End Dynamic Interaction, with Server-Side Rendering for SEO.&nbsp;&nbsp;
+										GitHub Copilot AI is used for Code Management and Intelligent Code Generation.<a href="https://github.com/Rsypertjr/code_storage/tree/presidentelect" target="_blank">Here is the Code.</a>
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
                     <Card style={{backgroundColor:'#F5DEB3'}}>
                         <img src={tictactoe} className="rounded mb-2" height="15%" width="15%" style={{margin:'1em 0 0 42.5%'}} alt="Tic Tac Toe Image"/> 
                         <Card.Body>             
@@ -522,6 +538,7 @@ export default function Portfolio(props){
                                 <Card.Text className="text-center">
                                     Programming I've done using:  Node.js based technologies of Vue, Angular, and React.  Some utilization of the Laravel MVC Framework and Node Express.
                                     Docker and Docker Compose Technologies used withth NPM and Webpack dependency management.
+                                    <br/>Added GitHub Copilot AI for Intelligent Code Generation and Management of Vercel-hosted Presidential Voting Analyzer App.
                                 </Card.Text>
                             </Card.Body>
                         </Card>
--- resources/js/Pages/Portfolio/TopCarousel.jsx
+++ resources/js/Pages/Portfolio/TopCarousel.jsx
@@ -1,8 +1,8 @@
-import React, { useState, useEffect} from 'react';
+import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import CarouselImage from '../../Components/CarouselImage';
 
-export default function TopCarousel(props){
+export default function TopCarousel(props){  
   
     return (
         <>
@@ -12,6 +12,16 @@ export default function TopCarousel(props){
                         <h4>Richard L. Sypert Jr's Work Portfolio</h4> 
                         <p className="top-desc rounded">This site shows my experience as a Software Developer, Technical Writer, and Manufacturing Engineer (resume).&nbsp;&nbsp;It is coded with Laravel11, React, Vite, and Bootstrap 5, as well as 
                             other technologies.&nbsp;&nbsp;Please view this carousel for information about my work history.</p>
+                    </Carousel.Caption>								
+                </Carousel.Item>	
+                <Carousel.Item>                    
+                    <Carousel.Caption>
+                        <h4>VSCode CoPilot AI-Assisted Next.js Presidential Voting Analyzer</h4> 
+                        <p className="top-desc rounded">Comprehensive Next.js application with Supabase SQL integration featuring interactive electoral data analysis, real-time voting
+							trend visualization, and demographic insights. Incorporates GitHub Copilot-powered code management with intelligent organization, automated tagging, 
+                            search functionality, and collaborative development.&nbsp;&nbsp;Link to code:&nbsp;<a href="https://github.com/Rsypertjr/code_storage/tree/presidentelect" target="_blank">Next.js-Presidential-Voting-Analyzer Code</a><br/>
+                            Link to app:&nbsp;<a href="https://codestorage.vercel.app/" target="_blank">Next.js Presidential Voting Analyzer</a>
+                        </p>
                     </Carousel.Caption>								
                 </Carousel.Item>				
                 <Carousel.Item>
@@ -34,7 +44,7 @@ export default function TopCarousel(props){
                     <Carousel.Caption>
                         <h4>Laravel-Mix MVC, React/Bootstrap/JQuery/ChartJs Component-based Front End</h4> 
                         <p className="top-desc rounded">Laravel (Mix) with React.js,&nbsp;React Router,&nbsp;Chart.js,&nbsp;and Bootstrap is used in this app:&nbsp;
-                        <a href="https://lar10reactmix.rsypertjr.net/votes-table" 
+                        <a href="https://laravelvotes.rsypertjr.net/votes-table" 
                         target="_blank">Laravel(Mix)/React/Chart.js/Bootstrap.js Vote Parser</a>.&nbsp;&nbsp;This app uses React Hooks like UseEffect and UseState for functional components.&nbsp;&nbsp;
                         Here is code link:&nbsp;
                         <a href="https://github.com/Rsypertjr/Laravel-React-Chartjs-Votes-Parser/tree/latest2" target="_blank">Laravel(Mix)/React/Chart.js/Bootstrap.js Vote Parser Code</a>.
@@ -45,14 +55,14 @@ export default function TopCarousel(props){
                     <Carousel.Caption>
                         <h4>Docker Laravel-9 Vite,React 17/Bootstrap/Chartjs Vote Parser Implementation</h4>
                         <p className="top-desc rounded">Also included is the code for another Vote Parer implementation using Docker/Docker-compose of Laravel 9 with Vite for React 17 javascript compilation. React Bootstrap and Chartjs is also utilized as before.
-                        Link to this code is <a href="https://github.com/Rsypertjr/Docker-Laravel-Vite-React-Bootstrap-Chartjs/tree/docker-laravel-vite" target="_blank">Docker Laravel-Vite/React Code</a><br/><a href="https://preselections.rsypertjr.net/" target="_blank">See App</a>
+                        Link to this code is <a href="https://github.com/Rsypertjr/Docker-Laravel-Vite-React-Bootstrap-Chartjs/tree/docker-laravel-vite" target="_blank">Docker Laravel-Vite/React Code</a>
                         </p>
                     </Carousel.Caption>								
                 </Carousel.Item>
                 <Carousel.Item>
                     <Carousel.Caption>
                         <h4>Vue3 Composition API, Vue3/Bootstrap/JQuery/ChartJs Component-based front end</h4> 
-                        <p  className="top-desc rounded">Vue 3 Composition API,&nbsp;Chart.js,&nbsp;and Bootstrap is used in this app:&nbsp;<a href="https://vue3chartjs.rsypertjr.net/" 
+                        <p  className="top-desc rounded">Vue 3 Composition API,&nbsp;Chart.js,&nbsp;and Bootstrap is used in this app:&nbsp;<a href="http://vuevotes.rsypertjr.net" 
                         target="_blank">Vue3(Composition API)/Chart.js/Bootstrap.js Vote Parser</a>.&nbsp;&nbsp;This app uses Vue3 Composition API which allows more function-based writing of components,&nbsp;inspired by React Hooks.&nbsp;&nbsp;
                         Here is code link:&nbsp;<a href="https://github.com/Rsypertjr/Vue3-Chartjs-Bootstrap.git" target="_blank">Vue3(Compostion API)/Chart.js/Bootstrap.js Vote Parser Code</a>.
                         </p>
